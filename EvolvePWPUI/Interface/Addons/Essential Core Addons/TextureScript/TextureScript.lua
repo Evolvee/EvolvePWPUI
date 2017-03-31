fa75e7d2b42c02a79050b9f7c92cbadda55688b0
Evolve <evolve.karel@gmail.com>
2017-03-31 20:37:10 +0200

    implemented auto-sell of junk gray items + AUTO REPAIR on "merchant window opening" event
    implemented auto-sell of junk gray items + AUTO REPAIR on "merchant
    window opening" event

--- EvolvePWPUI/Interface/Addons/Essential Core Addons/TextureScript/TextureScript.lua
+++ EvolvePWPUI/Interface/Addons/Essential Core Addons/TextureScript/TextureScript.lua
@@ -1020,6 +1020,46 @@ function plated:UPDATE_BATTLEFIELD_SCORE()
 	end
 end
 
+--auto-sell gray shits and try to repair(from guildbank if possible)
+
+local g = CreateFrame("Frame")
+g:RegisterEvent("MERCHANT_SHOW")
+
+g:SetScript("OnEvent", function()  
+    local bag, slot
+    for bag = 0, 4 do
+        for slot = 0, GetContainerNumSlots(bag) do
+            local link = GetContainerItemLink(bag, slot)
+            if link and (select(3, GetItemInfo(link)) == 0) then
+                UseContainerItem(bag, slot)
+            end
+        end
+    end
+
+    if(CanMerchantRepair()) then
+        local cost = GetRepairAllCost()
+        if cost > 0 then
+            local money = GetMoney()
+            if IsInGuild() then
+                local guildMoney = GetGuildBankWithdrawMoney()
+                if guildMoney > GetGuildBankMoney() then
+                    guildMoney = GetGuildBankMoney()
+                end
+                if guildMoney > cost and CanGuildBankRepair() then
+                    RepairAllItems(1)
+                    print(format("|cfff07100Repair cost covered by G-Bank: %.1fg|r", cost * 0.0001))
+                    return
+                end
+            end
+            if money > cost then
+                RepairAllItems()
+                print(format("|cffead000Repair cost: %.1fg|r", cost * 0.0001))
+            else
+                print("Not enough gold to cover the repair cost.")
+            end
+        end
+    end
+end)
 
 --login informing this UI was properly loaded
 ChatFrame1:AddMessage("PWP UI 2.2 Loaded successfully!",255,255,0)
